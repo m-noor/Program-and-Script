fa6688e0522e816968ac6b8b1ed3f240c6df7576
m-noor <36330156+m-noor@users.noreply.github.com>
2018-02-10 12:43:42 +0000

    Update Prog_and_script_Lesson2.py

--- Prog_and_script_Lesson2.py
+++ Prog_and_script_Lesson2.py
@@ -25,3 +25,17 @@ print("The last letter", last, "is number", lastno)
 print("Fibonacci number", x, "is", ans)
 print('\n' + '\n'  + "What does ord() function do?" + '\n')
 print(help(ord))
+
+# Week 1 output
+# My name is Mohamed, so the first and last letter of my name (M + D = 13 + 4) give the number 17. The 17th Fibonacci number is 1597.
+
+
+# Week 2 output
+#My surname is Noor.
+
+#The first letter N is number 78
+#The last letter r is number 114
+#Fibonacci number 192 is 5972304273877744135569338397692020533504
+
+#ord(c, /)
+#    Return the Unicode code point for a one-character string.
