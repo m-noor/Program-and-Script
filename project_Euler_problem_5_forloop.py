ad3805e22e722cb4efb7898ffa370a37379a4ba6
m-noor <36330156+m-noor@users.noreply.github.com>
2018-02-20 17:54:09 +0000

    Update project_Euler_problem_5_forloop.py

--- project_Euler_problem_5_forloop.py
+++ project_Euler_problem_5_forloop.py
@@ -1,7 +1,6 @@
 found_smallest_number=False
 
-# for a number to be divisible by all integers a to b, 
-# instead of 2 # this number should be divisible by all the prime numbers which is equivalent to 19*17*13*11*7*5*3*2
+# for a number to be divisible by all integers a to b, this number should be divisible by all the prime numbers which is equivalent to 19*17*13*11*7*5*3*2. so we should start our search from 9699690 (instead of 2) and then increment it by the same.
 # inspired by: http://www.mathblog.dk/project-euler-problem-5/
 
 smallest_number=9699690
@@ -21,4 +20,3 @@ while found_smallest_number == False:
             break # exit the inner for loop and restart all over with new number
 
 print("Smallest number = ", smallest_number) #232792560 for 1-20; 2520 for 1-10; the loop can be made slightly faster by setting smallest_number to 2520
-
